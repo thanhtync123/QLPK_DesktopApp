b7507f9b868bf497697ccf93e326afe1e53f6d5a
Nguyễn Thành Tỷ <22004071@st.vlute.edu.vn>
2025-07-28 22:42:47 -0700

    Enhance follow-up form with search and call actions
    Added a search textbox to filter patients by name and an action column to mark follow-up calls as completed. Improved DataGridView appearance, added color highlighting for follow-up dates, and enabled inline status updates for call actions.

--- QuanLyPhongKham/frm_followup.Designer.cs
+++ QuanLyPhongKham/frm_followup.Designer.cs
@@ -28,10 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dtgv = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.c_exam_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.c_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.c_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.c_address = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -39,45 +40,50 @@
             this.c_day_create = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.c_followup_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.c_state = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c_action = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txb_search = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
             this.SuspendLayout();
             // 
             // dtgv
             // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtgv.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dtgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtgv.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dtgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.None;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dtgv.ColumnHeadersHeight = 40;
             this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
             this.dtgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c_exam_id,
             this.c_id,
             this.c_name,
             this.c_address,
             this.c_phone,
             this.c_day_create,
             this.c_followup_date,
-            this.c_state});
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dtgv.DefaultCellStyle = dataGridViewCellStyle3;
-            this.dtgv.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.dtgv.Location = new System.Drawing.Point(12, 53);
+            this.c_state,
+            this.c_action});
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtgv.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dtgv.GridColor = System.Drawing.Color.Black;
+            this.dtgv.Location = new System.Drawing.Point(22, 56);
             this.dtgv.Name = "dtgv";
             this.dtgv.RowHeadersVisible = false;
-            this.dtgv.Size = new System.Drawing.Size(827, 385);
+            this.dtgv.Size = new System.Drawing.Size(1141, 540);
             this.dtgv.TabIndex = 0;
             this.dtgv.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
             this.dtgv.ThemeStyle.AlternatingRowsStyle.Font = null;
@@ -85,7 +91,7 @@
             this.dtgv.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
             this.dtgv.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
             this.dtgv.ThemeStyle.BackColor = System.Drawing.Color.White;
-            this.dtgv.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dtgv.ThemeStyle.GridColor = System.Drawing.Color.Black;
             this.dtgv.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
             this.dtgv.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.dtgv.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -100,21 +106,31 @@
             this.dtgv.ThemeStyle.RowsStyle.Height = 22;
             this.dtgv.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dtgv.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dtgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgv_CellClick);
+            // 
+            // c_exam_id
+            // 
+            this.c_exam_id.HeaderText = "Mã PK";
+            this.c_exam_id.Name = "c_exam_id";
+            this.c_exam_id.Visible = false;
             // 
             // c_id
             // 
             this.c_id.HeaderText = "Mã BN";
             this.c_id.Name = "c_id";
+            this.c_id.Width = 50;
             // 
             // c_name
             // 
             this.c_name.HeaderText = "Họ tên ";
             this.c_name.Name = "c_name";
+            this.c_name.Width = 200;
             // 
             // c_address
             // 
             this.c_address.HeaderText = "Địa chỉ";
             this.c_address.Name = "c_address";
+            this.c_address.Width = 240;
             // 
             // c_phone
             // 
@@ -125,34 +141,54 @@
             // 
             this.c_day_create.HeaderText = "Ngày khám";
             this.c_day_create.Name = "c_day_create";
+            this.c_day_create.Width = 200;
             // 
             // c_followup_date
             // 
             this.c_followup_date.HeaderText = "Ngày tái khám";
             this.c_followup_date.Name = "c_followup_date";
+            this.c_followup_date.Width = 147;
             // 
             // c_state
             // 
             this.c_state.HeaderText = "Trạng thái";
             this.c_state.Name = "c_state";
             // 
+            // c_action
+            // 
+            this.c_action.HeaderText = "Thao tác";
+            this.c_action.Name = "c_action";
+            // 
+            // txb_search
+            // 
+            this.txb_search.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_search.Location = new System.Drawing.Point(22, 25);
+            this.txb_search.Name = "txb_search";
+            this.txb_search.Size = new System.Drawing.Size(330, 26);
+            this.txb_search.TabIndex = 1;
+            this.txb_search.TextChanged += new System.EventHandler(this.txb_search_TextChanged);
+            // 
             // frm_followup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(863, 450);
+            this.ClientSize = new System.Drawing.Size(1198, 658);
+            this.Controls.Add(this.txb_search);
             this.Controls.Add(this.dtgv);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_followup";
             this.Text = "frm_followup";
             this.Load += new System.EventHandler(this.frm_followup_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Guna.UI2.WinForms.Guna2DataGridView dtgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_exam_id;
         private System.Windows.Forms.DataGridViewTextBoxColumn c_id;
         private System.Windows.Forms.DataGridViewTextBoxColumn c_name;
         private System.Windows.Forms.DataGridViewTextBoxColumn c_address;
@@ -160,5 +196,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn c_day_create;
         private System.Windows.Forms.DataGridViewTextBoxColumn c_followup_date;
         private System.Windows.Forms.DataGridViewTextBoxColumn c_state;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_action;
+        private System.Windows.Forms.TextBox txb_search;
     }
 }
--- QuanLyPhongKham/frm_followup.cs
+++ QuanLyPhongKham/frm_followup.cs
@@ -1,12 +1,9 @@
 ﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace QuanLyPhongKham
@@ -17,33 +14,111 @@ namespace QuanLyPhongKham
         {
             InitializeComponent();
         }
-
-        private void frm_followup_Load(object sender, EventArgs e)
+        private void LoadDTGV()
         {
+            dtgv.Rows.Clear(); //
             Db.ResetConnection();
+
             string query = $@"
+                SELECT 
+                    e.id,
+                    e.patient_id, 
+                    p.name, 
+                    p.address, 
+                    p.phone, 
+                    e.follow_up, 
+                    e.state,
+                    DATE_FORMAT(e.updated_at, '%d/%m/%Y %H:%i') AS updated_at
+                FROM examinations e
+                JOIN patients p ON e.patient_id = p.id
+                WHERE e.follow_up != 'Không' AND e.type = 'toa thuốc' and p.name LIKE '%{txb_search.Text}%'
+                ORDER BY e.updated_at DESC
+            ";
 
-
-                        ";
             Db.cmd = new MySqlCommand(query, Db.conn);
             Db.dr = Db.cmd.ExecuteReader();
+
             while (Db.dr.Read())
             {
                 int i = dtgv.Rows.Add();
                 DataGridViewRow drr = dtgv.Rows[i];
-                drr.Cells["c_id"].Value = Db.dr["id"];
+                drr.Cells["c_exam_id"].Value = Db.dr["id"];
+                drr.Cells["c_id"].Value = Db.dr["patient_id"];
                 drr.Cells["c_name"].Value = Db.dr["name"];
                 drr.Cells["c_address"].Value = Db.dr["address"];
                 drr.Cells["c_phone"].Value = Db.dr["phone"];
                 drr.Cells["c_day_create"].Value = Db.dr["updated_at"];
                 drr.Cells["c_followup_date"].Value = Db.dr["follow_up"];
                 drr.Cells["c_state"].Value = Db.dr["state"];
-       
+
+                // Xử lý highlight cột follow_up theo ngày
+                string followUpStr = Db.dr["follow_up"].ToString();
+                if (DateTime.TryParseExact(followUpStr, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime followUpDate))
+                {
+                    DateTime today = DateTime.Today;
+                    TimeSpan diff = followUpDate.Date - today;
+
+                    var cell = drr.Cells["c_followup_date"];
+                    cell.Style.ForeColor = Color.Black;
+                    cell.Style.Font = new Font(dtgv.Font, FontStyle.Bold);
+                    if (diff.Days == 0)
+                    {
+                        // Hôm nay: đỏ nhạt
+                        cell.Style.BackColor = Color.FromArgb(255, 204, 204);
+                    }
+                    else if (diff.Days > 0 && diff.Days <= 3)
+                    {
+                        // Trong 3 ngày tới: vàng nhạt
+                        cell.Style.BackColor = Color.FromArgb(255, 255, 192);
+                    }
+                    else if (diff.Days < 0)
+                    {
+                        // Trễ hẹn: cam nhạt
+                        cell.Style.BackColor = Color.FromArgb(255, 229, 204);
+                    }
+
+                }
+
+                // Xử lý trạng thái
+                string state = Db.dr["state"].ToString().Trim().ToLower();
+                if (state == "đã gọi")
+                {
+                    drr.Cells["c_action"].Value = "✔ Đã gọi";
+                    drr.Cells["c_action"].Style.ForeColor = Color.Green;
+                }
+                else
+                {
+                    drr.Cells["c_action"].Value = "📞 Gọi";
+                    drr.Cells["c_action"].Style.ForeColor = Color.Blue;
+                }
             }
+
             Db.dr.Close();
+            Db.dr.Close();
+        }
+        private void frm_followup_Load(object sender, EventArgs e)
+        {
+            LoadDTGV();
+        }
+
+        private void dtgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || dtgv.Columns[e.ColumnIndex].Name != "c_action") return;
+
+            string id = dtgv.Rows[e.RowIndex].Cells["c_id"].Value?.ToString();
+            if (string.IsNullOrEmpty(id)) return;
+
+            string query = $"UPDATE examinations SET state = 'Đã gọi' WHERE patient_id = '{id}' AND type = 'toa thuốc' AND follow_up != 'Không'";
+            Db.ExecuteNonQuery(query);
+            dtgv.Rows[e.RowIndex].Cells["c_state"].Value = "Đã gọi";
+            dtgv.Rows[e.RowIndex].Cells["c_action"].Value = "✅";
 
         }
 
+        private void txb_search_TextChanged(object sender, EventArgs e)
+        {
+            LoadDTGV();
 
+        }
     }
 }
