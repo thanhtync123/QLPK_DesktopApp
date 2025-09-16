c0c41d30ca4da3576150a4e98639c1fe871778db
Nguyễn Thành Tỷ <22004071@st.vlute.edu.vn>
2025-09-16 21:16:44 +0700

    Bắt đầu fix rdlc động

--- QuanLyPhongKham/frm_login.cs
+++ QuanLyPhongKham/frm_login.cs
@@ -20,7 +20,7 @@ namespace QuanLyPhongKham
 
         private void btn_login_Click(object sender, EventArgs e)
         {
-            //1
+            //12112
             try
             {
                 Db.ResetConnection();
