051e53b902bb22c40dc229e8eb9414b21b3e3748
Nguyễn Thành Tỷ <22004071@st.vlute.edu.vn>
2025-09-17 17:01:25 +0700

    Xong para phiếu thuốc

--- QuanLyPhongKham/frm_report_med.cs
+++ QuanLyPhongKham/frm_report_med.cs
@@ -90,7 +90,12 @@ namespace QuanLyPhongKham
                     new ReportParameter("txb_taikham", _taikham ?? ""),
                     new ReportParameter("txb_songaythuoc", _songaythuoc ?? ""),
                     new ReportParameter("txb_dtname", CurrentUser.UserName ?? ""),
-                    new ReportParameter("pr_sign", filepath ?? "")
+                    new ReportParameter("pr_sign", filepath ?? ""),
+                    new ReportParameter("txb_businesstype", "PHÒNG KHÁM ĐA KHOA"),
+                    new ReportParameter("txb_businessname", "THÚY NGA"),
+                    new ReportParameter("txb_businessservice", "SIÊU ÂM MÀU - KHÁM BỆNH"),
+                    new ReportParameter("txb_businessaddress", "123 Lê Lợi, Quận 1, TP. HCM"),
+                    new ReportParameter("txb_businessphone", "0931111222")
                 };
 
                 reportViewer1.LocalReport.SetParameters(parameters);
